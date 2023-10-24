1f341e333bd4a5783b1e13bc3bc78bde52222d24
nbn22385 <nbn22385@gmail.com>
2023-10-24 14:48:28 -0400

    folding init.vim

--- init.lua
+++ init.lua
@@ -1,3 +1,4 @@
+-- [[ Plugin manager ]] {{{
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
@@ -17,63 +18,66 @@ vim.opt.rtp:prepend(lazypath)
 require('lazy').setup({
   { import = 'custom.plugins' },
 }, {})
+-- }}}
 
--- [[ Setting options ]]
+-- [[ Options ]] {{{
 
 -- vim.opt.path=$PWD/**                   -- Find all files under this directory
-vim.opt.breakindent = true                -- Enable break indent
-vim.opt.clipboard = 'unnamedplus'         -- Sync clipboard between OS and Neovim.
-vim.opt.completeopt = 'menuone,noselect'  -- Set completeopt to have a better completion experience
-vim.opt.fillchars:append("eob: ")         -- Empty lines below the end of a buffer
-vim.opt.fillchars:append("fold: ")        -- Filling 'foldtext'
-vim.opt.fillchars:append("foldopen:┬")    -- Mark the beginning of a fold
-vim.opt.fillchars:append("foldsep:│")     -- Open fold middle character
-vim.opt.foldopen:append({ 'jump' })       -- Open a fold if we jump inside it
-vim.opt.hlsearch = true                   -- Set highlight on search
-vim.opt.ignorecase = true                 -- Case-insensitive searching
-vim.opt.iskeyword:remove({ '-' })         -- Treat dash separated words as a word text object
-vim.opt.listchars:append({ eol = '↵' })   -- Characters to show in 'list' mode
-vim.opt.listchars:append({ tab = '▸ ' })  -- Characters to show in 'list' mode
-vim.opt.listchars:append({ trail = '·' }) -- Characters to show in 'list' mode    
-vim.opt.matchpairs:append({"<:>"})        -- Add angle brackets to list of matching pairs
-vim.opt.mouse = 'a'                       -- Enable mouse mode
-vim.opt.mouse = a                         -- Enable use of the mouse in all modes
-vim.opt.nrformats = ''                    -- Only recognize decimal numbers for increment/decrement
-vim.opt.number = true                     -- Make line numbers default
-vim.opt.number = true                     -- Show line numbers
-vim.opt.scrolloff = 5                     -- Show at least 5 lines above and below the cursor
-vim.opt.shiftround = true                 -- >> indents to next multiple of 'shiftwidth'
-vim.opt.shiftwidth = 2                    -- >> indents by 2 spaces
-vim.opt.shortmess = 'acFIT'               -- Customize vim messages
-vim.opt.showmode = false                  -- Don't show current mode in command-line
-vim.opt.signcolumn = 'yes'                -- Keep signcolumn on by default
-vim.opt.smartcase = true                  -- Ignore ignorecase if capital letter used in search
-vim.opt.softtabstop = 2                   -- Tab key indents by 2 spaces
-vim.opt.splitbelow = true                 -- Open new split panes below
-vim.opt.splitright = true                 -- Open new split panes to the right
-vim.opt.tabstop = 2                       -- Number of spaces that a <Tab> counts for
-vim.opt.termguicolors = true              -- Enables 24-bit RGB color 
-vim.opt.textwidth = 0                     -- Disable maximum text width
-vim.opt.timeoutlen = 300                  -- Time for a mapped sequence to complete
-vim.opt.undofile = true                   -- Save undo history
-vim.opt.updatetime = 250                  -- Shorter CursorHold delay
+vim.opt.breakindent = true -- Enable break indent
+vim.opt.clipboard = 'unnamedplus' -- Sync clipboard between OS and Neovim.
+vim.opt.completeopt = 'menuone,noselect' -- Set completeopt to have a better completion experience
+vim.opt.fillchars:append("eob: ") -- Empty lines below the end of a buffer
+vim.opt.fillchars:append("fold: ") -- Filling 'foldtext'
+vim.opt.fillchars:append("foldopen:┬") -- Mark the beginning of a fold
+vim.opt.fillchars:append("foldsep:│") -- Open fold middle character
+vim.opt.foldopen:append({ 'jump' }) -- Open a fold if we jump inside it
+vim.opt.hlsearch = true -- Set highlight on search
+vim.opt.ignorecase = true -- Case-insensitive searching
+vim.opt.iskeyword:remove({ '-' }) -- Treat dash separated words as a word text object
+vim.opt.listchars:append({ eol = '↵' }) -- Characters to show in 'list' mode
+vim.opt.listchars:append({ tab = '▸ ' }) -- Characters to show in 'list' mode
+vim.opt.listchars:append({ trail = '·' }) -- Characters to show in 'list' mode
+vim.opt.matchpairs:append({ "<:>" }) -- Add angle brackets to list of matching pairs
+vim.opt.mouse = 'a' -- Enable mouse mode
+vim.opt.mouse = a -- Enable use of the mouse in all modes
+vim.opt.nrformats = '' -- Only recognize decimal numbers for increment/decrement
+vim.opt.number = true -- Make line numbers default
+vim.opt.number = true -- Show line numbers
+vim.opt.scrolloff = 5 -- Show at least 5 lines above and below the cursor
+vim.opt.shiftround = true -- >> indents to next multiple of 'shiftwidth'
+vim.opt.shiftwidth = 2 -- >> indents by 2 spaces
+vim.opt.shortmess = 'acFIT' -- Customize vim messages
+vim.opt.showmode = false -- Don't show current mode in command-line
+vim.opt.signcolumn = 'yes' -- Keep signcolumn on by default
+vim.opt.smartcase = true -- Ignore ignorecase if capital letter used in search
+vim.opt.softtabstop = 2 -- Tab key indents by 2 spaces
+vim.opt.splitbelow = true -- Open new split panes below
+vim.opt.splitright = true -- Open new split panes to the right
+vim.opt.tabstop = 2 -- Number of spaces that a <Tab> counts for
+vim.opt.termguicolors = true -- Enables 24-bit RGB color
+vim.opt.textwidth = 0 -- Disable maximum text width
+vim.opt.timeoutlen = 300 -- Time for a mapped sequence to complete
+vim.opt.undofile = true -- Save undo history
+vim.opt.updatetime = 250 -- Shorter CursorHold delay
 vim.opt.wildcharm = vim.api.nvim_replace_termcodes('<Tab>', true, true, true):byte() -- Allow wildmenu expansion from macros/remaps
-vim.opt.wildignorecase = true             -- Case-insensitive completion of commands, filenames
-vim.opt.wildmode = "longest:full,full"    -- Wildmenu completion mode
-vim.opt.wrap = false                      -- Don't automatically wrap long lines to the window
+vim.opt.wildignorecase = true -- Case-insensitive completion of commands, filenames
+vim.opt.wildmode = "longest:full,full" -- Wildmenu completion mode
+vim.opt.wrap = false -- Don't automatically wrap long lines to the window
 
 if vim.fn.executable('rg') then
   vim.opt.grepprg = 'rg --vimgrep --hidden --ignore-case'
   vim.opt.grepformat:prepend('%f:%l:%c:%m')
 end
+-- }}}
 
--- [[ Commands ]]
+-- [[ Commands ]] {{{
 
 vim.api.nvim_create_user_command('Grep', function(_)
   -- execute 'silent grep! <args> | redraw! | cwindow
 end, { desc = 'Grep and open the quickfix window with results' })
+-- }}}
 
--- [[ Basic Keymaps ]]
+-- [[ Keymaps ]] {{{
 
 -- Keymaps for better default experience
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>')
@@ -99,7 +103,7 @@ vim.keymap.set('n', '<Leader>=', 'gg=G<C-o><C-o>')
 
 -- Open lazygit in a new tab
 vim.keymap.set('n', '<Leader>G', ':$tab terminal lazygit<CR>')
-vim.api.nvim_create_autocmd('TermOpen', {pattern = {'*'}, command = 'startinsert'})
+vim.api.nvim_create_autocmd('TermOpen', { pattern = { '*' }, command = 'startinsert' })
 
 -- Quicker split resizing
 vim.keymap.set('n', '<Leader><Left>', ':vertical resize -3<CR>')
@@ -176,16 +180,32 @@ vim.keymap.set('x', 'p', '_dP')
 
 -- Replace all occurrences of visual selection
 vim.keymap.set('x', '<Leader>h', 'y:%s/<C-r>0//g<Left><Left>')
+-- }}}
 
--- [[ Highlight on yank ]]
--- See `:help vim.highlight.on_yank()`
-local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
+-- [[ Autocommands ]] {{{
+
+-- Highlight on yank
+local init_group = vim.api.nvim_create_augroup('init', { clear = true })
 vim.api.nvim_create_autocmd('TextYankPost', {
   callback = function()
     vim.highlight.on_yank()
   end,
-  group = highlight_group,
+  group = init_group,
   pattern = '*',
 })
 
--- vim: ts=2 sts=2 sw=2 et
+-- When editing a file, always jump to the last known cursor position.
+vim.api.nvim_create_autocmd('BufReadPost', {
+  group = init_group,
+  callback = function(args)
+    local valid_line = vim.fn.line([['"]]) >= 1 and vim.fn.line([['"]]) < vim.fn.line('$')
+    local not_commit = vim.b[args.buf].filetype ~= 'commit'
+
+    if valid_line and not_commit then
+      vim.cmd([[normal! g`"]])
+    end
+  end,
+})
+-- }}}
+
+-- vim: fdm=marker ts=2 sts=2 sw=2 et
