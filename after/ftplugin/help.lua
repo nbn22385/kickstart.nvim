c4d05d230429d2fe89696fdb777264f67d7b3286
Nathan Novick <nathan.novick@lmco.com>
2023-11-15 13:11:51 -0500

    ftplugins

---
+++ after/ftplugin/help.lua
@@ -0,0 +1,2 @@
+vim.keymap.set('n', 'q', ':helpclose<CR>', { desc = 'Close help window', buffer = true })
+vim.keymap.set('n', '<CR>', '<C-]>', { desc = 'Go to help tag', buffer = true })
---
+++ after/ftplugin/qf.lua
@@ -0,0 +1,4 @@
+vim.keymap.set('n', 'q', ':cclose<CR>', { desc = 'Close quickfix window', buffer = true })
+
+vim.opt_local.signcolumn = 'no'
+vim.opt_local.wrap = true
