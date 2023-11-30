9e9599cf008c229b7e4fea7039dcbf931ee1c6e8
nbn22385 <nbn22385@gmail.com>
2023-11-30 10:12:44 -0500

    lsp keymaps

--- lua/custom/plugins/lsp.lua
+++ lua/custom/plugins/lsp.lua
@@ -29,14 +29,13 @@ return {
 				vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
 			end
 
-			nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
-			nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
-
-			nmap('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
-			nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
-			nmap('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
-			nmap('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
-			nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
+			nmap('<localleader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
+			nmap('<localleader>gd', vim.lsp.buf.lsp_definitions, '[G]oto [D]efinition')
+			nmap('<localleader>gi', vim.lsp.buf.lsp_implementations, '[G]oto [I]mplementation')
+			nmap('<localleader>gr', vim.lsp.buf.lsp_references, '[G]oto [R]eferences')
+			nmap('<localleader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
+			nmap('<localleader>gD', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
+			nmap('<localleader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
 			-- nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
 
 			-- See `:help K` for why this keymap
@@ -44,7 +43,7 @@ return {
 			nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
 
 			-- Lesser used LSP functionality
-			nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+			-- nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
 			-- nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
 			-- nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
 			-- nmap('<leader>wl', function()
