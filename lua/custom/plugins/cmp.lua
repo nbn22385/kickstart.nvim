87b8f7ff3bd7a78ddb6f3837c42fc1c3e1fd6f66
nbn22385 <nbn22385@gmail.com>
2024-03-21 16:24:57 -0400

    update cmp config

--- lua/custom/plugins/cmp.lua
+++ lua/custom/plugins/cmp.lua
@@ -1,62 +1,75 @@
 return {
-	-- Autocompletion
-	'hrsh7th/nvim-cmp',
+	"hrsh7th/nvim-cmp",
+	version = false, -- last release is way too old
+	event = "InsertEnter",
 	dependencies = {
-		-- Snippet Engine & its associated nvim-cmp source
-		'L3MON4D3/LuaSnip',
-		'saadparwaiz1/cmp_luasnip',
-
-		-- Adds LSP completion capabilities
-		'hrsh7th/cmp-nvim-lsp',
-
-		-- Adds a number of user-friendly snippets
-		'rafamadriz/friendly-snippets',
+		"hrsh7th/cmp-nvim-lsp",
+		"hrsh7th/cmp-buffer",
+		"hrsh7th/cmp-path",
+		"onsails/lspkind-nvim",
+		"L3MON4D3/LuaSnip",
+		"saadparwaiz1/cmp_luasnip",
 	},
-	config = function()
-		local cmp = require 'cmp'
-		local luasnip = require 'luasnip'
-		require('luasnip.loaders.from_vscode').lazy_load()
-		luasnip.config.setup {}
-
-		cmp.setup {
-			snippet = {
-				expand = function(args)
-					luasnip.lsp_expand(args.body)
-				end,
+	opts = function()
+		vim.api.nvim_set_hl(0, "CmpGhostText", { link = "Comment", default = true })
+		local cmp = require("cmp")
+		local defaults = require("cmp.config.default")()
+		local lspkind = require('lspkind')
+		return {
+			completion = {
+				autocomplete = false,
+				completeopt = "menu,menuone,noinsert",
 			},
-			mapping = cmp.mapping.preset.insert {
-				['<C-n>'] = cmp.mapping.select_next_item(),
-				['<C-p>'] = cmp.mapping.select_prev_item(),
-				['<C-d>'] = cmp.mapping.scroll_docs(-4),
-				['<C-f>'] = cmp.mapping.scroll_docs(4),
-				['<C-Space>'] = cmp.mapping.complete {},
-				['<CR>'] = cmp.mapping.confirm {
+			mapping = cmp.mapping.preset.insert({
+				["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+				["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+				["<C-b>"] = cmp.mapping.scroll_docs(-4),
+				["<C-f>"] = cmp.mapping.scroll_docs(4),
+				["<C-Space>"] = cmp.mapping.complete(),
+				["<C-e>"] = cmp.mapping.abort(),
+				["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+				["<S-CR>"] = cmp.mapping.confirm({
 					behavior = cmp.ConfirmBehavior.Replace,
 					select = true,
+				}), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+				["<C-CR>"] = function(fallback)
+					cmp.abort()
+					fallback()
+				end,
+			}),
+			snippet =
+				{
+					-- REQUIRED - you must specify a snippet engine
+					expand = function(args)
+						-- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+						require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+						-- require('snippy').expand_snippet(args.body) -- For `snippy` users.
+						-- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+						-- vim.snippet.expand(args.body) -- For native neovim snippets (Neovim v0.10+)
+					end,
+			},
+			sources = cmp.config.sources({
+				{ name = "nvim_lsp" },
+				{ name = "path" },
+				}, {
+					{ name = "buffer" },
+			}),
+			formatting = {
+				format = lspkind.cmp_format(),
+			},
+			experimental = {
+				ghost_text = {
+					hl_group = "CmpGhostText",
 				},
-				['<Tab>'] = cmp.mapping(function(fallback)
-					if cmp.visible() then
-						cmp.select_next_item()
-					elseif luasnip.expand_or_locally_jumpable() then
-						luasnip.expand_or_jump()
-					else
-						fallback()
-					end
-				end, { 'i', 's' }),
-				['<S-Tab>'] = cmp.mapping(function(fallback)
-					if cmp.visible() then
-						cmp.select_prev_item()
-					elseif luasnip.locally_jumpable(-1) then
-						luasnip.jump(-1)
-					else
-						fallback()
-					end
-				end, { 'i', 's' }),
 			},
-			sources = {
-				{ name = 'nvim_lsp' },
-				{ name = 'luasnip' },
-			},
+			sorting = defaults.sorting,
 		}
+	end,
+	---@param opts cmp.ConfigSchema
+	config = function(_, opts)
+		for _, source in ipairs(opts.sources) do
+			source.group_index = source.group_index or 1
+		end
+		require("cmp").setup(opts)
 	end,
 }
